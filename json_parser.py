ca707be2eda60e2a3eb1b81eac1afe599b7d1d29
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-05-16 00:30:18 +0300

    Add json parser and main logic

--- json_parser.py
+++ json_parser.py
@@ -1,2 +1,21 @@
 import json
 
+
+def load_json(path):
+    with open(path, 'r') as json_file:
+        users = json.load(json_file)
+    return users
+
+
+def get_personal_info(users):
+    personal_info = []
+    for user in users:
+        user_info = {
+            'name': user['name'],
+            'gender': user['gender'],
+            'address': user['address'],
+            'age': user['age']
+        }
+        personal_info.append(user_info)
+    return personal_info
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import csv_parser
+import json_parser
+
+if __name__ == "__main__":
+    loaded_json = json_parser.load_json('users.json')
+    personal_info = json_parser.get_personal_info(loaded_json)
+    print(personal_info)
