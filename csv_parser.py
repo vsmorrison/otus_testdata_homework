162feb5f6d91b2a8e0f20d14d738f5d15dbf182b
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-05-16 00:44:29 +0300

    Add csv_parser

--- csv_parser.py
+++ csv_parser.py
@@ -1,1 +1,16 @@
 import csv
+
+
+def get_books(path):
+    books_info = []
+    with open(path, newline='') as csv_file:
+        books = csv.DictReader(csv_file)
+        for book in books:
+            book_info = {
+                "title": book['Title'],
+                "author": book['Author'],
+                "pages": book['Pages'],
+                "genre": book['Genre']
+            }
+            books_info.append(book_info)
+    return books_info
--- main.py
+++ main.py
@@ -5,3 +5,5 @@ if __name__ == "__main__":
     loaded_json = json_parser.load_json('users.json')
     personal_info = json_parser.get_personal_info(loaded_json)
     print(personal_info)
+    books = csv_parser.get_books('books.csv')
+    print(books)
