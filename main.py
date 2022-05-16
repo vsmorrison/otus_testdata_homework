9874cd01e47e69d052190cc08032dc6121e08926
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-05-16 14:16:21 +0300

    Add logic for giving users books and making result file for it

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import csv_parser
 import json_parser
+import books_to_users
 
 if __name__ == "__main__":
     users = json_parser.get_users('users.json')
-    print(users)
     books = csv_parser.get_books('books.csv')
-    print(books)
+    users_w_books = books_to_users.give_books_to_users(users, books)
+    books_to_users.write_users_to_json('result.json', users_w_books)
